12acd664cac440e046d650c8a0e41a82f431f03b
Kieu Anh <kieuanh.tran@nfq.asia>
2022-03-30 22:39:57 +0700

    design dashboard home admin ui

---
+++ src/pages/Admin/DashBoard/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Header from '../Header';
+import Sidebar from '../Sidebar';
+import WelcomeBanner from './sections/WelcomeBanner';
+
+function Dashboard() {
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    return (
+        <div className="flex h-screen overflow-hidden">
+
+        {/* Sidebar */}
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+  
+        {/* Content area */}
+        <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+  
+          {/*  Site header */}
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+          <main>
+            <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+              {/* Welcome banner */}
+              <WelcomeBanner />
+            </div>
+          </main>
+  
+  
+        </div>
+      </div>
+    );
+}
+
+export default Dashboard;
