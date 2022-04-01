eb6e892cb4ac770e89a098e52fee732d8c444f0f
nguyet-nguyen <nguyet.nguyen@nfq.asia>
2022-04-01 14:47:27 +0700

    commit api sign up form

--- src/pages/Admin/Products/index.js
+++ src/pages/Admin/Products/index.js
@@ -16,7 +16,6 @@ const Products = () => {
                 <main>
                     <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
                         <ProductsTable/>
-
                     </div>
                 </main>
             </div>
