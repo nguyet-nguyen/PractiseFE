32d478a00e2c8908c94dc2923a0824f8c23b2e5b
Kieu Anh <kieuanh.tran@nfq.asia>
2022-04-01 18:09:06 +0700

    update shopping cart interface

--- src/pages/LandingPages/ShoppingCart/sections/Cart.js
+++ src/pages/LandingPages/ShoppingCart/sections/Cart.js
@@ -2,243 +2,162 @@ import React from "react";
 
 const ShoppingCart = () => {
   return (
-        <div
-          className="flex lg:flex-row flex-col justify-around"
-          id="cart"
-        >
-          <div
-            className="lg:w-1/2 md:w-8/12 w-full lg:px-8 lg:py-8 md:px-6 px-4 py-4 bg-white dark:bg-gray-800 lg:h-screen h-auto"
-            
-          >
-            <div className="flex items-center text-gray-500 hover:text-gray-600 dark:text-white cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon icon-tabler icon-tabler-chevron-left"
-                width="16"
-                height="16"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <polyline points="15 6 9 12 15 18" />
-              </svg>
-              <p className="text-md pl-2 leading-none dark:hover:text-gray-200">
-                Back
+    <>
+      <div className="px-4 md:px-3">
+        <p className="lg:text-4xl text-3xl font-black leading-10 text-[#63584c] dark:text-white py-3">
+          Shopping Cart
+        </p>
+      </div>
+      <div className="flex lg:flex-row flex-col" id="cart">
+        <div className="lg:w-2/3 md:w-8/12 w-full px-4 py-2 bg-white dark:bg-gray-800 lg:h-screen h-auto">
+          <div className="md:flex items-strech py-8 md:py-10 lg:py-8 border-t border-gray-100">
+            <div className="md:w-4/12 2xl:w-1/4 w-full">
+              <img
+                src="https://media.gucci.com/style/HEXEAF2DC_Center_0_0_800x800/1643239845/688610_XJD7W_9061_002_100_0000_Light-Jersey-zip-jacket-with-Web.jpg"
+                alt="Dress1"
+                className="h-full object-center object-cover md:block hidden"
+              />
+              <img
+                src="https://media.gucci.com/style/HEXEAF2DC_Center_0_0_800x800/1643239845/688610_XJD7W_9061_002_100_0000_Light-Jersey-zip-jacket-with-Web.jpg"
+                alt="Dress1"
+                className="md:hidden w-full h-full object-center object-cover"
+              />
+            </div>
+            <div className="md:pl-3 lg:ml-7 md:w-8/12 2xl:w-3/4 flex flex-col justify-center">
+              <p className="text-md leading-3 text-gray-800 dark:text-white md:pt-0 pt-4">
+                ID12
               </p>
-            </div>
-            <p className="lg:text-4xl text-3xl font-black leading-10 text-gray-800 dark:text-white pt-3">
-              Bag
-            </p>
-            <div className="md:flex items-strech py-8 md:py-10 lg:py-8 border-t border-gray-50">
-              <div className="md:w-4/12 2xl:w-1/4 w-full">
-                <img
-                  src="https://i.ibb.co/SX762kX/Rectangle-36-1.png"
-                  alt="Black Leather Bag"
-                  className="h-full object-center object-cover md:block hidden"
-                />
-                <img
-                  src="https://i.ibb.co/g9xsdCM/Rectangle-37.pngg"
-                  alt="Black Leather Bag"
-                  className="md:hidden w-full h-full object-center object-cover"
-                />
+              <div className="flex items-center justify-between w-full py-3">
+                <p className="text-lg font-black leading-none text-gray-800 dark:text-white">
+                  Light Jersey zip jacket
+                </p>
+                <select
+                  aria-label="Select quantity"
+                  className="py-2 px-1 border border-gray-200 mr-6 focus:outline-none dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white"
+                >
+                  <option>01</option>
+                  <option>02</option>
+                  <option>03</option>
+                </select>
               </div>
-              <div className="md:pl-3 lg:ml-7 md:w-8/12 2xl:w-3/4 flex flex-col justify-center">
-                <p className="text-xs leading-3 text-gray-800 dark:text-white md:pt-0 pt-4">
-                  RF293
+              <div className="flex items-center pb-8">
+                <p className="text-sm font-semibold leading-3 text-gray-500 dark:text-white pr-3">
+                  White
                 </p>
-                <div className="flex items-center justify-between w-full pt-1">
-                  <p className="text-base font-black leading-none text-gray-800 dark:text-white">
-                    North wolf bag
-                  </p>
-                  <select
-                    aria-label="Select quantity"
-                    className="py-2 px-1 border border-gray-200 mr-6 focus:outline-none dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white"
-                  >
-                    <option>01</option>
-                    <option>02</option>
-                    <option>03</option>
-                  </select>
-                </div>
-                <p className="text-xs leading-3 text-gray-600 dark:text-white pt-2">
-                  Height: 10 inches
+                <p className="text-sm font-semibold leading-3 text-gray-500 dark:text-white border-l border-gray-300 pl-3">
+                  Large
                 </p>
-                <p className="text-xs leading-3 text-gray-600 dark:text-white py-4">
-                  Color: Black
-                </p>
-                <p className="w-96 text-xs leading-3 text-gray-600 dark:text-white">
-                  Composition: 100% calf leather
-                </p>
-                <div className="flex items-center justify-between pt-5">
-                  <div className="flex itemms-center">
-                    <p className="text-xs leading-3 underline text-gray-800 dark:text-white cursor-pointer">
-                      Add to favorites
-                    </p>
-                    <p className="text-xs leading-3 underline text-red-500 pl-5 cursor-pointer">
-                      Remove
-                    </p>
-                  </div>
-                  <p className="text-base font-black leading-none text-gray-800 dark:text-white">
-                    ,000
-                  </p>
-                </div>
               </div>
-            </div>
-            <div className="md:flex items-strech py-8 md:py-10 lg:py-8 border-t border-gray-50">
-              <div className="md:w-4/12 2xl:w-1/4 w-full">
-                <img
-                  src="https://i.ibb.co/c6KyDXN/Rectangle-5-1.png"
-                  alt="Gray Sneakers"
-                  className="h-full object-center object-cover md:block hidden"
-                />
-                <img
-                  src="https://i.ibb.co/yVSpYqx/Rectangle-6.png"
-                  alt="Gray Sneakers"
-                  className="md:hidden w-full h-full object-center object-cover"
-                />
-              </div>
-              <div className="md:pl-3 lg:ml-7 md:w-8/12 2xl:w-3/4 flex flex-col justify-center">
-                <p className="text-xs leading-3 text-gray-800 dark:text-white md:pt-0 pt-4">
-                  RF293
+
+              <div className="flex items-center pt-7">
+                <p className="text-md font-bold leading-3 text-gray-800 dark:text-white">
+                  $20.00
                 </p>
-                <div className="flex items-center justify-between w-full pt-1">
-                  <p className="text-base font-black leading-none text-gray-800 dark:text-white">
-                    LW sneakers
-                  </p>
-                  <select
-                    aria-label="Select quantity"
-                    className="py-2 px-1 border border-gray-200 mr-6 focus:outline-none dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white"
-                  >
-                    <option>01</option>
-                    <option>02</option>
-                    <option>03</option>
-                  </select>
-                </div>
-                <p className="text-xs leading-3 text-gray-600 dark:text-white pt-2">
-                  Height: 10 inches
+                <p className="text-sm leading-3 underline text-red-500 pl-5 cursor-pointer">
+                  Remove
                 </p>
-                <p className="text-xs leading-3 text-gray-600 dark:text-white py-4">
-                  Color: Black
-                </p>
-                <p className="w-96 text-xs leading-3 text-gray-600 dark:text-white">
-                  Composition: 100% calf leather
-                </p>
-                <div className="flex items-center justify-between pt-5">
-                  <div className="flex itemms-center">
-                    <p className="text-xs leading-3 underline text-gray-800 dark:text-white cursor-pointer">
-                      Add to favorites
-                    </p>
-                    <p className="text-xs leading-3 underline text-red-500 pl-5 cursor-pointer">
-                      Remove
-                    </p>
-                  </div>
-                  <p className="text-base font-black leading-none text-gray-800 dark:text-white">
-                    ,000
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="md:flex items-strech py-8 md:py-10 lg:py-8 border-t border-gray-50">
-              <div className="md:w-4/12 2xl:w-1/4 w-full">
-                <img
-                  src="https://i.ibb.co/6gzWwSq/Rectangle-20-1.png"
-                  alt="Black Leather Purse"
-                  className="h-full object-center object-cover md:block hidden"
-                />
-                <img
-                  src="https://i.ibb.co/TTnzMTf/Rectangle-21.png"
-                  alt="Black Leather Purse"
-                  className="md:hidden w-full h-full object-center object-cover"
-                />
-              </div>
-              <div className="md:pl-3 lg:ml-7 md:w-8/12 2xl:w-3/4 flex flex-col justify-center">
-                <p className="text-xs leading-3 text-gray-800 dark:text-white md:pt-0 pt-4">
-                  RF293
-                </p>
-                <div className="flex items-center justify-between w-full">
-                  <p className="text-base font-black leading-none text-gray-800 dark:text-white">
-                    Luxe card holder
-                  </p>
-                  <select
-                    aria-label="Select quantity"
-                    className="py-2 px-1 border border-gray-200 mr-6 focus:outline-none dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white"
-                  >
-                    <option>01</option>
-                    <option>02</option>
-                    <option>03</option>
-                  </select>
-                </div>
-                <p className="text-xs leading-3 text-gray-600 dark:text-white pt-2">
-                  Height: 10 inches
-                </p>
-                <p className="text-xs leading-3 text-gray-600 dark:text-white py-4">
-                  Color: Black
-                </p>
-                <p className="w-96 text-xs leading-3 text-gray-600 dark:text-white">
-                  Composition: 100% calf leather
-                </p>
-                <div className="flex items-center justify-between pt-5">
-                  <div className="flex itemms-center">
-                    <p className="text-xs leading-3 underline text-gray-800 dark:text-white cursor-pointer">
-                      Add to favorites
-                    </p>
-                    <p className="text-xs leading-3 underline text-red-500 pl-5 cursor-pointer">
-                      Remove
-                    </p>
-                  </div>
-                  <p className="text-base font-black leading-none text-gray-800 dark:text-white">
-                    ,000
-                  </p>
-                </div>
               </div>
             </div>
           </div>
-          <div className="lg:w-96 md:w-8/12 w-full bg-gray-100 dark:bg-gray-900 h-full">
-            <div className="flex flex-col h-auto lg:px-8 md:px-7 px-4 lg:py-15 md:py-8 py-6 justify-between overflow-y-auto">
-              <div>
-                <p className="lg:text-4xl text-3xl font-black leading-9 text-gray-800 dark:text-white">
-                  Summary
+          <div className="md:flex items-strech py-8 md:py-10 lg:py-8 border-t border-gray-100">
+            <div className="md:w-4/12 2xl:w-1/4 w-full">
+              <img
+                src="https://media.gucci.com/style/HEXEAF2DC_Center_0_0_800x800/1643239849/688613_XJD6C_9061_002_100_0000_Light-Jersey-shorts-with-Web.jpg"
+                alt="Light-Jersey-short"
+                className="h-full object-center object-cover md:block hidden"
+              />
+              <img
+                src="https://media.gucci.com/style/HEXEAF2DC_Center_0_0_800x800/1643239849/688613_XJD6C_9061_002_100_0000_Light-Jersey-shorts-with-Web.jpg"
+                alt="Light-Jersey-short"
+                className="md:hidden w-full h-full object-center object-cover"
+              />
+            </div>
+            <div className="md:pl-3 lg:ml-7 md:w-8/12 2xl:w-3/4 flex flex-col justify-center">
+              <p className="text-md leading-3 text-gray-800 dark:text-white md:pt-0 pt-4">
+                ID12
+              </p>
+              <div className="flex items-center justify-between w-full py-3">
+                <p className="text-lg font-black leading-none text-gray-800 dark:text-white">
+                  Light Jersey short
                 </p>
-                <div className="flex items-center justify-between pt-16">
-                  <p className="text-base leading-none text-gray-800 dark:text-white">
-                    Subtotal
-                  </p>
-                  <p className="text-base leading-none text-gray-800 dark:text-white">
-                    ,000
-                  </p>
-                </div>
-                <div className="flex items-center justify-between pt-5">
-                  <p className="text-base leading-none text-gray-800 dark:text-white">
-                    Shipping
-                  </p>
-                  <p className="text-base leading-none text-gray-800 dark:text-white"></p>
-                </div>
-                <div className="flex items-center justify-between pt-5">
-                  <p className="text-base leading-none text-gray-800 dark:text-white">
-                    Tax
-                  </p>
-                  <p className="text-base leading-none text-gray-800 dark:text-white"></p>
-                </div>
+                <select
+                  aria-label="Select quantity"
+                  className="py-2 px-1 border border-gray-200 mr-6 focus:outline-none dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white"
+                >
+                  <option>01</option>
+                  <option>02</option>
+                  <option>03</option>
+                </select>
               </div>
-              <div>
-                <div className="flex items-center pb-6 justify-between lg:pt-5 pt-20">
-                  <p className="text-2xl leading-normal text-gray-800 dark:text-white">
-                    Total
-                  </p>
-                  <p className="text-2xl font-bold leading-normal text-right text-gray-800 dark:text-white">
-                    ,240
-                  </p>
-                </div>
-                <button className="text-base leading-none w-full py-5 bg-gray-800 border-gray-800 border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white dark:hover:bg-gray-700">
-                  Checkout
-                </button>
+              <div className="flex items-center pb-8">
+                <p className="text-sm font-semibold leading-3 text-gray-500 dark:text-white pr-3">
+                  Black
+                </p>
+                <p className="text-sm font-semibold leading-3 text-gray-500 dark:text-white border-l border-gray-300 pl-3">
+                  Large
+                </p>
+              </div>
+
+              <div className="flex items-center pt-7">
+                <p className="text-md font-bold leading-3 text-gray-800 dark:text-white">
+                  $20.00
+                </p>
+                <p className="text-sm leading-3 underline text-red-500 pl-5 cursor-pointer">
+                  Remove
+                </p>
               </div>
             </div>
           </div>
         </div>
+
+        <div className="lg:w-1/3 lg:ml-20 md:w-8/12 w-96 bg-gray-100 dark:bg-gray-900 h-full">
+          <div className="flex flex-col h-auto lg:px-8 md:px-7 px-4 lg:py-16 md:py-8 py-6 justify-between overflow-y-auto">
+            <div>
+              <p className="lg:text-2xl text-xl font-bold leading-9 text-gray-800 dark:text-white">
+                Order Summary
+              </p>
+              <div className="flex items-center justify-between pt-12 pb-5">
+                <p className="text-base font-semibold leading-none text-gray-500 dark:text-white">
+                  Subtotal
+                </p>
+                <p className="text-base  font-bold leading-none text-gray-700 dark:text-white">
+                  $20
+                </p>
+              </div>
+              <div className="flex items-center justify-between py-5 border-t border-gray-300">
+                <p className="text-base  font-semibold leading-none text-gray-500 dark:text-white">
+                  Shipping
+                </p>
+                <p className="text-base  font-bold leading-none text-gray-700 dark:text-white">
+                  $10
+                </p>
+              </div>
+              <div className="flex items-center justify-between py-5 border-t border-gray-300">
+                <p className="text-base  font-semibold leading-none text-gray-500 dark:text-white">
+                  Tax
+                </p>
+                <p className="text-base  font-bold leading-none text-gray-700 dark:text-white">
+                  $10
+                </p>
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center pb-6 justify-between lg:pt-12 py-5 pt-20 border-t border-gray-300">
+                <p className="text-xl font-bold leading-normal text-gray-800 dark:text-white">
+                  Total
+                </p>
+                <p className="text-xl font-bold leading-normal text-right text-gray-800 dark:text-white">
+                  $240
+                </p>
+              </div>
+              <button className="text-lg leading-none w-full py-5 bg-amber-600 font-bold rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-800 text-white dark:hover:bg-amber-700">
+                Checkout
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
