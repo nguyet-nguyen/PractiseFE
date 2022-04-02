5c2b8f6623251ec97d97995aaf05f7c7a438a0d0
nguyet-nguyen <nguyet.nguyen@nfq.asia>
2022-04-02 13:43:11 +0700

    commit table product and user

--- src/pages/Admin/Products/sections/ProductsTable.js
+++ src/pages/Admin/Products/sections/ProductsTable.js
@@ -1,5 +1,83 @@
+import $ from 'jquery'
+import dt from 'datatables.net'
+
+$.DataTable = dt
+import React, {Component, useEffect} from "react";
+
+
 const PeoductsTable = () => {
-    return (<div>products admin page</div>);
+    useEffect(() => {
+            $('#usersTable').DataTable({
+                "bDestroy": true,
+                dom: 'Blfrtip',
+                buttons: [
+                    'copy', 'excel', 'pdf'
+                ]
+            });
+        }
+    )
+
+
+    let data = [
+        {
+            id: 1,
+            name: "Tiger Nixon",
+            category: "System Architect",
+            aa: "Edinburgh",
+            amount: "5421",
+            date: "2011/04/25",
+            price: "$320,800",
+        },
+        {
+            id: 2,
+            name: "Minh Nguyet",
+            category: "Front-end Developer",
+            aa: "Edinburgh",
+            amount: "5421",
+            date: "2011/04/25",
+            price: "$640,000",
+        }
+    ];
+
+    return (
+        <table id="usersTable" className="display">
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                data.map(d => (
+                    <tr>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{d.name}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{d.category}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{d.aa}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{d.amount}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{d.date}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{d.price}</td>
+                    </tr>
+                ))
+            }
+
+            </tbody>
+            <tfoot>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+            </tfoot>
+        </table>
+    )
 }
 
 export default PeoductsTable;
--- src/pages/Admin/Users/index.js
+++ src/pages/Admin/Users/index.js
@@ -1,28 +1,36 @@
-import React, {useState} from "react";
+import React, {Component, useState} from "react";
 import Sidebar from "../Sidebar";
 import Header from "../Header";
 import UsersTable from "./sections/UserTable";
 
 const Users = () => {
 
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-    return (
-        <div className="flex h-screen overflow-hidden">
-            {/* Sidebar */}
-            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
-            {/* Content area */}
-            <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-                {/*  Site header */}
-                <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
-                <main>
-                    <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-                        <UsersTable/>
+    const dataSet = [
+        ["Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800"],
 
-                    </div>
-                </main>
+    ];
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+        return (
+            <div className="flex h-screen overflow-hidden">
+                {/* Sidebar */}
+                <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
+                {/* Content area */}
+                <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+                    {/*  Site header */}
+                    <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
+                    <main>
+                        <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+                            <table className="display" width="100%" >
+                            </table>
+                            <UsersTable />
+                        </div>
+                    </main>
+                </div>
             </div>
-        </div>
-    )
+        );
+
+
 }
 
 export default Users;
