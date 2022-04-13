526a7e5247b896e13f350c82e27f5dc507e15d46
nguyet-nguyen <nguyet.nguyen@nfq.asia>
2022-04-13 10:48:32 +0700

    commit change process bar

--- src/pages/Admin/Products/sections/ProductsTable.js
+++ src/pages/Admin/Products/sections/ProductsTable.js
@@ -206,9 +206,9 @@ const ProductsTable = () => {
                       type="button"
                       data-mdb-ripple="true"
                       data-mdb-ripple-color="light"
-                      className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs
-                                leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700
-                                focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition
+                      className="inline-block px-6 py-2.5 bg-indigo-600 text-white font-medium text-xs
+                                leading-tight uppercase rounded shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-indigo-700
+                                focus:shadow-lg focus:outline-none focus:ring-0 active:bg-indio-800 active:shadow-lg transition
                                 duration-150 ease-in-out">Add Product
                 </Link>
                 <div className="xl:w-96">
