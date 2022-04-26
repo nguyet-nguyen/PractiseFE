a68dbf6462a82a564c254bc82db6eb104955b177
nguyet-nguyen <nguyet.nguyen@nfq.asia>
2022-04-26 15:34:01 +0700

    commit fix active sidebar admin

--- src/pages/Admin/Sidebar/index.js
+++ src/pages/Admin/Sidebar/index.js
@@ -122,16 +122,16 @@ function Sidebar({
               </li>
              
               {/* Users */}
-              <SidebarLinkGroup activecondition={pathname.includes('team')}>
+              <SidebarLinkGroup activecondition={pathname === '/admin/users-list/admin' || pathname === '/admin/users-list/buyer'}>
                 {(handleClick, open) => {
                   return (
                       <React.Fragment>
-                        <a href="#0" className={`block truncate transition duration-150 ${pathname.includes('team') && 'hover:text-white text-white'}`} onClick={(e) => { e.preventDefault(); sidebarExpanded ? handleClick() : setSidebarExpanded(true) }}>
+                        <a href="#0" className={`block truncate transition duration-150`} onClick={(e) => { e.preventDefault(); sidebarExpanded ? handleClick() : setSidebarExpanded(true) }}>
                           <div className="flex items-center justify-between">
                             <div className="flex items-center">
                               <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
-                                <path className={`fill-current text-amber-600 ${pathname.includes('team') && 'text-amber-500'}`} d="M18.974 8H22a2 2 0 012 2v6h-2v5a1 1 0 01-1 1h-2a1 1 0 01-1-1v-5h-2v-6a2 2 0 012-2h.974zM20 7a2 2 0 11-.001-3.999A2 2 0 0120 7zM2.974 8H6a2 2 0 012 2v6H6v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5H0v-6a2 2 0 012-2h.974zM4 7a2 2 0 11-.001-3.999A2 2 0 014 7z" />
-                                <path className={`fill-current text-amber-500 ${pathname.includes('team') && 'text-amber-600'}`} d="M12 6a3 3 0 110-6 3 3 0 010 6zm2 18h-4a1 1 0 01-1-1v-6H6v-6a3 3 0 013-3h6a3 3 0 013 3v6h-3v6a1 1 0 01-1 1z" />
+                                <path className={`fill-current text-amber-600 ${(pathname === '/admin/users-list/admin' || pathname === '/admin/users-list/buyer') && 'text-amber-700'}`} d="M18.974 8H22a2 2 0 012 2v6h-2v5a1 1 0 01-1 1h-2a1 1 0 01-1-1v-5h-2v-6a2 2 0 012-2h.974zM20 7a2 2 0 11-.001-3.999A2 2 0 0120 7zM2.974 8H6a2 2 0 012 2v6H6v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5H0v-6a2 2 0 012-2h.974zM4 7a2 2 0 11-.001-3.999A2 2 0 014 7z" />
+                                <path className={`fill-current text-amber-500 ${(pathname === '/admin/users-list/admin' || pathname === '/admin/users-list/buyer') && 'text-amber-700'}`} d="M12 6a3 3 0 110-6 3 3 0 010 6zm2 18h-4a1 1 0 01-1-1v-6H6v-6a3 3 0 013-3h6a3 3 0 013 3v6h-3v6a1 1 0 01-1 1z" />
                               </svg>
                               <span className="text-sm font-semibold ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Accounts</span>
                             </div>
@@ -145,14 +145,16 @@ function Sidebar({
                         </a>
                         <div className="lg:hidden lg:sidebar-expanded:block 2xl:block">
                           <ul className={`pl-9 mt-1 ${!open && 'hidden'}`}>
-                            <li className="mb-1 last:mb-0">
+                            <li className={`p-1.5 last:mb-0  ${(pathname === '/admin/users-list/admin') && 'bg-amber-500'}`}>
                               <NavLink end to="/admin/users-list/admin" className="block text-slate-500 hover:text-slate-800 transition duration-150 truncate">
-                                <span className="text-sm font-semibold lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Admins</span>
+                                <span className={`text-sm font-semibold lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200
+                                ${(pathname === '/admin/users-list/admin') && 'text-white'}`}>Admins</span>
                               </NavLink>
                             </li>
-                            <li className="mb-1 last:mb-0">
+                            <li className={`p-1.5 last:mb-0  ${pathname === '/admin/users-list/buyer' && 'bg-amber-500'}`}>
                               <NavLink end to="/admin/users-list/buyer" className="block text-slate-500 hover:text-slate-800 transition duration-150 truncate">
-                                <span className="text-sm font-semibold lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Users</span>
+                                 <span className={`text-sm font-semibold lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200
+                                ${ pathname === '/admin/users-list/buyer' && 'text-white'}`}>Users</span>
                               </NavLink>
                             </li>
                           </ul>
